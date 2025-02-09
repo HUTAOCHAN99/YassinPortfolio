10ba06bce906b8bcb2959edf4d2b25a443e83c96
HUTAOCHAN99 <ahmadzhofir1808@gmail.com>
2025-02-09 21:37:02 +0700

    perbaikan

--- assets/js/card-gallery.js
+++ assets/js/card-gallery.js
@@ -1,27 +1,30 @@
 class CardGallery extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback() {
-        this.shadowRoot.innerHTML = `
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
             <link rel="stylesheet" href="assets/css/app.css">
             <link rel="stylesheet" href="assets/css/theme.css">
             <style>
-                .modal {
-                    display: none;
-                    position: fixed;
-                    z-index: 1000;
-                    left: 0;
-                    top: 0;
-                    width: 100%;
-                    height: 100%;
-                    background: rgba(0, 0, 0, 0.8);
-                    align-items: center;
-                    justify-content: center;
-                }
+            .modal {
+             display: none;
+             position: fixed; 
+             z-index: 1000;
+             left: 50%;
+             top: 50%;
+             width: 100%;
+             height: 100%;
+             background: rgba(0, 0, 0, 0.8);
+             display: none;
+             align-items: center;
+             justify-content: center;
+             transform: translate(-50%, -50%); /* Pastikan posisi tetap di tengah */
+            }
+
                 .modal img {
                     max-width: 90%;
                     max-height: 90%;
@@ -51,21 +54,23 @@ class CardGallery extends HTMLElement {
             </div>
         `;
 
-        this.addEventListeners();
-    }
+    this.addEventListeners();
+  }
 
-    generateCards() {
-        const images = [
-            "./assets/img/portfolio1.png",
-            "./assets/img/portfolio2.png",
-            "./assets/img/portfolio3.png",
-            "./assets/img/portfolio4.png",
-            "./assets/img/portfolio5.png",
-            "./assets/img/portfolio6.png",
-            "./assets/img/portfolio7.png",
-            "./assets/img/portfolio8.png",
-        ];
-        return images.map(img => `
+  generateCards() {
+    const images = [
+      "./assets/img/portfolio1.png",
+      "./assets/img/portfolio2.png",
+      "./assets/img/portfolio3.png",
+      "./assets/img/portfolio4.png",
+      "./assets/img/portfolio5.png",
+      "./assets/img/portfolio6.png",
+      "./assets/img/portfolio7.png",
+      "./assets/img/portfolio8.png",
+    ];
+    return images
+      .map(
+        (img) => `
             <div class="card card-pin">
                 <img class="card-img" src="${img}" alt="Card image">
                 <div class="overlay">
@@ -77,34 +82,36 @@ class CardGallery extends HTMLElement {
                     </div>
                 </div>
             </div>
-        `).join('');
-    }
+        `
+      )
+      .join("");
+  }
 
-    addEventListeners() {
-        const modal = this.shadowRoot.getElementById("imageModal");
-        const modalImg = this.shadowRoot.getElementById("modalImg");
-        const closeModal = this.shadowRoot.querySelector(".modal-close");
+  addEventListeners() {
+    const modal = this.shadowRoot.getElementById("imageModal");
+    const modalImg = this.shadowRoot.getElementById("modalImg");
+    const closeModal = this.shadowRoot.querySelector(".modal-close");
 
-        this.shadowRoot.querySelectorAll(".open-modal").forEach(link => {
-            link.addEventListener("click", (e) => {
-                e.preventDefault();
-                modal.style.display = "flex"; 
-                modalImg.src = e.target.closest("a").dataset.src;
-            });
-        });
+    this.shadowRoot.querySelectorAll(".open-modal").forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.style.display = "flex";
+        modalImg.src = e.target.closest("a").dataset.src;
+      });
+    });
 
-        // Event untuk menutup modal
-        closeModal.addEventListener("click", () => {
-            modal.style.display = "none";
-        });
+    // Event untuk menutup modal
+    closeModal.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
 
-        // Tutup modal saat klik di luar gambar
-        modal.addEventListener("click", (e) => {
-            if (e.target === modal) {
-                modal.style.display = "none";
-            }
-        });
-    }
+    // Tutup modal saat klik di luar gambar
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  }
 }
 
 customElements.define("card-gallery", CardGallery);
